30b2b76003b9ea084b8de0660d19d97d753ccd88
natepage <nathan.page@eonx.com>
2021-08-20 07:31:15 +1000

    Add dummy test for now

---
+++ plugins/PaymentsPlugin/tests/DummyTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EonX\PaymentsPlugin\Tests;
+
+final class DummyTest extends AbstractTestCase
+{
+    public function testDummy(): void
+    {
+        self::assertTrue(true);
+    }
+}
